97ae14b1aec3935e13305d6f3aafb6ba7a171e92
Ayushi <aprakash162001@gmail.com>
2020-11-30 23:52:26 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Draggable from "react-draggable";
+import { v4 as uuidv4 } from "uuid";
+var randomColor = require("randomcolor");
 
 function App() {
+  const [item, setItem] = useState(""); //array destructuring
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("items")) || []
+  );
+
+  const newitem = () => {
+    if (item.trim() !== "") {
+      const newitem = {
+        id: uuidv4(),
+        item: item,
+        color: randomColor({ luminosity: "light" }),
+        defaultPos: { x: 100, y: 0 },
+      };
+      //add this new item object to the items array
+      setItems((items) => [...items, newitem]);
+      //reset item value to empty string
+      setItem("");
+    } else {
+      alert("Enter an item");
+      setItem("");
+    }
+  };
+
+  const keyPress = (e) => {
+    var code = e.keyPress || e.which;
+    if (code === 13) {
+      newitem();
+    }
+  };
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
+
+  const updatePos = (data, index) => {
+    let newArr = [...items];
+    newArr[index].defaultPos = { x: data.x, y: data.y };
+    setItems(newArr);
+  };
+
+  const deleteNote = (id) => {
+    setItems(items.filter((item) => item.id !== id));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="new-item">
+        <input
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+          placeholder="Enter something..."
+          onKeyPress={(e) => keyPress(e)}
+        />
+        <button onClick={newitem}>ENTER</button>
+      </div>
+      {items.map((item, index) => {
+        return (
+          <Draggable
+            key={item.id}
+            defaultPosition={item.defaultPos}
+            onStop={(e, data) => {
+              updatePos(data, index);
+            }}
+          >
+            <div style={{ backgroundColor: item.color }} className="box">
+              {`${item.item}`}
+              <button id="delete" onClick={(e) => deleteNote(item.id)}>
+                X
+              </button>
+            </div>
+          </Draggable>
+        );
+      })}
     </div>
   );
 }
